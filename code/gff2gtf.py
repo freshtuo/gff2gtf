05f3612ffe866250ca3c1894ac19ad3b8771bb6a
Tuo Zhang <taz2008@gc5.med.cornell.edu>
2019-04-18 12:50:56 -0400

    initilize script

---
+++ code/gff2gtf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# gff2gtf.py
+# convert gff gene annotation file to gtf
+# Author: Tuo Zhang
+# Version: 1.0
+# NEW: first version
+# 
+
+import sys
+
+
+
+print "Complete!"
+
